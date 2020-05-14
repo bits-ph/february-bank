8364b61cb6de23908f732d584f6c382767f5d6cc
Kristoffer Mendoza <toffmendozame@Toffs-MacBook-Pro.local>
2020-05-14 23:43:55 +0800

    Apply new layout

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,18 @@
-$photoAlbum = $('.js-masonry');
-$photoAlbum.masonry({
-	itemSelector: '.js-masonry-item', // use a separate class for itemSelector, other than .col-
-	columnWidth: '.js-masonry-sizer',
-	percentPosition: true,
-	transitionDuration: 0
-});
-
 $sectionLinks = $(".js-section-link")
 $sections = $(".js-section")
+
+$("#photos").slick({
+	arrows: false,
+	speed: 0,
+	infinite: false
+});
+
+$("#pages").slick({
+	arrows: false,
+	speed: 0,
+	infinite: false
+});
+
 
 sectionId = window.top.location.hash.substr(1);
 if (sectionId.length != 0) {
@@ -16,10 +21,6 @@ if (sectionId.length != 0) {
 	if ($section.length > 0) {
 		$("a[href='"+sectionId+"']").addClass("active");
 		$section.removeClass("d-none");
-		if (sectionId == "#photo-album") {
-			$photoAlbum.masonry('layout')
-			console.log("Reloaded masonry")
-		}
 	}
 }
 else {
@@ -29,18 +30,74 @@ else {
 	$section.removeClass("d-none");
 }
 
+$spotifyPlayer = $("#spotify-player")
+$lyrics = $("#lyrics")
+
+function selectTrack($track) {
+	$spotifyPlayer.attr("src", $track.data("src"));
+	$lyrics.html($track.html())
+}
+
+$track1 = $("#playlist").children().first();
+selectTrack($track1);
+$track1.addClass("active");
+
 $("a[href*='#']").click(function() {
-	 event.preventDefault();
-	sectionId = $(this).attr('href');
+	event.preventDefault();
+	sectionId = $(this).attr("href");
 	$section = $sections.filter(sectionId)
 	if ($section.length > 0) {
 		$sectionLinks.removeClass("active");
 		$(this).addClass("active");
 		$sections.addClass("d-none");
 		$section.removeClass("d-none");
-		if (sectionId == "#photo-album") {
-			$photoAlbum.masonry('layout')
-			console.log("Reloaded masonry")
-		}
+		if (sectionId == "#about") { $("#pages").slick("refresh"); }
+		else if (sectionId == "#gallery") { $("#photos").slick("refresh"); }
+	}
+});
+
+$(".js-slick-controls a.js-control-next").click(function() {
+	$control = $(this);
+	var refId = $control.parents(".js-slick-controls").data("ref");
+	var $ref = $("#"+refId);
+	$ref.slick("slickNext").on("afterChange", function(event, slick, currentSlide, nextSlide){
+		$control.siblings("a.js-control-prev").removeClass("control--disabled");
+		if (slick.currentSlide >= (slick.slideCount - 1)) { $control.addClass("control--disabled"); }
+	});
+});
+
+$(".js-slick-controls a.js-control-prev").click(function() {
+	$control = $(this);
+	var refId = $control.parents(".js-slick-controls").data("ref");
+	var $ref = $("#"+refId);
+	$ref.slick("slickPrev").on("afterChange", function(event, slick, currentSlide, nextSlide){
+		$control.siblings("a.js-control-next").removeClass("control--disabled");
+		if (slick.currentSlide == 0) { $control.addClass("control--disabled"); }
+	});
+});
+
+$(".js-music-controls a.js-control-next").click(function() {
+	$control = $(this);
+	$current = $("#playlist > li.active")
+	$next = $current.next()
+	if ($next.length > 0) {
+		$current.removeClass("active")
+		selectTrack($next);
+		$next.addClass("active")
+		$control.siblings("a.js-control-prev").removeClass("control--disabled");
+		if ($next.is(":last-child")) { $control.addClass("control--disabled") }
+	}
+});
+
+$(".js-music-controls a.js-control-prev").click(function() {
+	$control = $(this);
+	$current = $("#playlist > li.active")
+	$prev = $current.prev()
+	if ($prev.length > 0) {
+		$current.removeClass("active")
+		selectTrack($prev);
+		$prev.addClass("active")
+		$control.siblings("a.js-control-next").removeClass("control--disabled");
+		if ($prev.is(":first-child")) { $control.addClass("control--disabled") }
 	}
 });
